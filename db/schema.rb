23e4289b90a0624d01b88ce0e60bc754e4022533
abdulrhman haider <abd097@hotmail.com>
2018-05-27 03:41:51 +0200

    Test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180519032333) do
+ActiveRecord::Schema.define(version: 20180519223221) do
 
   create_table "genders", force: :cascade do |t|
     t.string "name"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 20180519032333) do
     t.string "phone"
     t.integer "gender_id"
     t.string "name_manger"
+    t.boolean "gender_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
