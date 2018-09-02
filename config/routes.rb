86a8cc9ba66064f6bfe9e351454fd90047883ef3
abdulrhman haider <abd097@hotmail.com>
2018-09-03 00:56:29 +0200

    Convert About

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,8 @@ Rails.application.routes.draw do
  resources :home
 
 
+ get '/about-us' => 'home#about'
   root :to => "home#index"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 end
