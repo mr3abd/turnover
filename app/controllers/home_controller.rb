ea23c951e8c25c69b22079155ae9efe8b8952730
abdulrhman haider <abd097@hotmail.com>
2018-09-13 13:16:56 +0200

    form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   layout "home"
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
 
   def index
 
@@ -8,4 +9,20 @@ class HomeController < ApplicationController
   def about
 
   end
+
+def resource_name
+  :user
+end
+
+def resource
+  @resource ||= User.new
+end
+
+def resource_class
+  User
+end
+
+def devise_mapping
+  @devise_mapping ||= Devise.mappings[:user]
+end
 end
