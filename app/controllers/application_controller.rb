758c93d21b6c9b2fd770f1bf7b4e5052b051e0f1
abdulrhman haider <abd097@hotmail.com>
2018-09-13 14:51:56 +0200

    form css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
- before_action :set_locale
+  before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
  def default_url_options(options={})
   { :locale => I18n.locale }
 end
  private
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:name , :phone ,:activity, :workers , :name_manger_acc , :jobpostion , :name_manger])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:name , :phone, :activity, :workers, :name_manger_acc , :jobpostion , :name_manger])
+ end
  def set_locale
    I18n.locale = params[:locale] || I18n.default_locale
 
