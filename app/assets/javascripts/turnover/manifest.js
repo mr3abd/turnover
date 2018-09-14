305e5f2bcfb82a647dd38854d866e11a20f8fb4d
abdulrhman haider <abd097@hotmail.com>
2018-09-14 19:10:08 +0200

    Abilty

--- app/assets/javascripts/turnover/manifest.js
+++ app/assets/javascripts/turnover/manifest.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require turnover/custom.js
 
 $("form").on("keypress", function (e) {
